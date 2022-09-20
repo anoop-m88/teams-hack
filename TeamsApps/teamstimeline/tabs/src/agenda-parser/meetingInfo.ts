5b423a95e820ebfe53d372b3d4df73bddc1026df
Anoop Muraleedharan <anmura@microsoft.com>
2022-09-20 15:16:02 -0700

    created parser for parsing agenda

---
+++ TeamsApps/teamstimeline/tabs/src/agenda-parser/meetingInfo.ts
@@ -0,0 +1,18 @@
+export const getMeetingInfo = (): string => {
+    return `
+<<Agenda>>
+System Health   
+   Front-end services - Veeru -5m
+   Back-end services  - Srek-5m
+   Incidents-Mayank -5m
+   Performance-Harman-5m
+Roll-out
+   Home page-Vishnu-5m
+   PDP/Category pages-Anoop-5m
+   Search-Prasanna-5m
+New investments
+   Configurator-Vishnu-10m
+   Personalization-Sanjeev-5m
+<</Agenda>>
+    `;
+}
---
+++ TeamsApps/teamstimeline/tabs/src/models/agenda.types.ts
@@ -0,0 +1,5 @@
+export interface Agenda {
+    text: string;
+    timeInMinutes: number;
+    speaker: string;
+}
