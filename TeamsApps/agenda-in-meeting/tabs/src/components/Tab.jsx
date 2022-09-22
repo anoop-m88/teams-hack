37914e28435ee275f592222d92580ef9ac9965c1
Lu Ouyang <luouyang@microsoft.com>
2022-09-22 03:02:42 -0700

    line working

--- TeamsApps/agenda-in-meeting/tabs/src/components/Tab.jsx
+++ TeamsApps/agenda-in-meeting/tabs/src/components/Tab.jsx
@@ -1,85 +1,147 @@
 import React from "react";
 import * as microsoftTeams from "@microsoft/teams-js";
-import MediaQuery from 'react-responsive';
-import './App.css';
-import * as parser from '../agenda-parser/parser';
+import MediaQuery from "react-responsive";
+import "./App.css";
+import * as parser from "../agenda-parser/parser";
 
-
-const TimelineItem = ({ data, marker, last }) => (
+const TimelineItem = ({ data, marker, last, index, nowDone }) => (
   <div>
-      {/* @ts-expect-error */}
-    <div className={`agenda-item ${data?.status} ${data?.level===2? 'level2': 'level1'}`} style={{'--length': data?.timeInMinutes}}>
-      
-      <div className='time'>{marker[0]+=last}</div>
-      <span className="topic">{data?.text || 'End'}</span>
+    {/* @ts-expect-error */}
+    <div
+      className={`agenda-item ${nowDone===index? 'yay':''} ${data?.status} ${
+        data?.level === 2 ? "level2" : "level1"
+      }`}
+      style={{ "--length": data?.timeInMinutes, "--overlay": data?.overlay }}
+    >
+      <div className="time">{(marker[0] += last)}</div>
+      <span className="topic">{data?.text || "End"}</span>
       <p className="speaker">{data?.speaker}</p>
     </div>
   </div>
 );
 
 class Tab extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.nowDone = -1;
+    this.intervalId = undefined;
     this.state = {
-      context: {}
-    }
+      context: {},
+      timelineData: parser
+        .getAgendaItems()
+        .map((v) => ({ ...v, level: 2, status: "not-done", overlay: 0 })),
+    };
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     microsoftTeams.initialize();
     // Get the user context from Teams and set it in the state
     microsoftTeams.getContext((context, error) => {
       this.setState({
-        context: context
+        context: context,
       });
     });
-    microsoftTeams.meeting.getAppContentStageSharingCapabilities((err, appContentStageSharingCapabilities) => {
-      if (appContentStageSharingCapabilities) {
-        //alert(appContentStageSharingCapabilities.doesAppHaveSharePermission);
-        this.setState({canShowStage: appContentStageSharingCapabilities.doesAppHaveSharePermission})
+    microsoftTeams.meeting.getAppContentStageSharingCapabilities(
+      (err, appContentStageSharingCapabilities) => {
+        if (appContentStageSharingCapabilities) {
+          //alert(appContentStageSharingCapabilities.doesAppHaveSharePermission);
+          this.setState({
+            canShowStage:
+              appContentStageSharingCapabilities.doesAppHaveSharePermission,
+          });
+        } else if (err) {
+          alert(err.message);
+          console.log("Error with API call.");
+        }
       }
-      else if (err) {
-        alert(err.message);
-        console.log("Error with API call.");
-      }
-    });
+    );
     // Next steps: Error handling using the error object
+
+    const getStatus = (item, i) => {
+      if (i === 0 && item.status === "not-done") return "in-progress";
+      if (item.overlay >= item.timeInMinutes) return "done";
+      return item.status;
+    };
+    const tick = () => {
+      this.setState((old) => {
+        const newState = {
+          ...old,
+          timelineData: old.timelineData.map((v, i) => ({
+            ...v,
+            overlay: v.status === "in-progress" ? v.overlay + 1 : v.overlay,
+            status: getStatus(v, i),
+            // nowDone: v.status === 'in-progress' && getStatus(v,i) === 'done'? i: -1,
+          })),
+        };
+        newState.timelineData.forEach((v,i,arr)=>{
+          if (i>0 && arr[i-1].status==='done' && v.status==='not-done') v.status = 'in-progress';
+          if (old.timelineData[i].status === 'in-progress' &&  v.status==='done')
+            newState.nowDone = i;
+        })
+        console.dir(newState);
+        return newState;
+        
+      });
+    };
+    this.intervalId = setInterval(tick, 2000);
   }
 
-   
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  componentDidUpdate(){
+    if (this.state.timelineData[this.state.timelineData.length-1].status === 'done')
+      clearInterval(this.intervalId)
+  }
 
   render() {
-    let meetingId = this.state.context['meetingId'] ?? "";
-    let userPrincipleName = this.state.context['userPrincipalName'] ?? "";
-    const timelineData = parser.getAgendaItems();
+    let meetingId = this.state.context["meetingId"] ?? "";
+    let userPrincipleName = this.state.context["userPrincipalName"] ?? "";
+
     const accumulation = [0];
-    timelineData.forEach(v=>{v.level = 2; v.status='not-done'; })
-console.log(timelineData)
 
     return (
-    <div className="agenda-hack-container">
-      <h1>In-meeting app sample</h1>
-      <h3>Principle Name:</h3>
-      <p>{userPrincipleName}</p>
-      <h3>Meeting ID:</h3>
-      <p>{meetingId}</p>
-      <p>doesAppHaveSharePermission={this.state.canShowStage}</p>
- 
-      <MediaQuery maxWidth={2000}>
-        <h3>This is the side panel</h3>
+      <div className="agenda-hack-container">
+        <h1>In-meeting app sample</h1>
+        <h3>Principle Name:</h3>
+        <p>{userPrincipleName}</p>
+        <h3>Meeting ID:</h3>
+        <p>{meetingId}</p>
+        <p>doesAppHaveSharePermission={this.state.canShowStage}</p>
 
-        {timelineData.length > 0 && (
-          <div className="timeline-container">
-              {timelineData.map((data, idx, arr) => (
-                  <TimelineItem data={data} marker={accumulation} last={idx>0? arr[idx-1].timeInMinutes: 0} key={idx} />
-              ))}
-              <TimelineItem data={null} marker={accumulation} last={timelineData[timelineData.length-1].timeInMinutes} key={timelineData.length} />
-          </div>
-        )}
-      </MediaQuery>
-    </div>
+        <MediaQuery maxWidth={2000}>
+          <h3>This is the side panel</h3>
+
+          {this.state.timelineData.length > 0 && (
+            <div className="timeline-container">
+              {this.state.timelineData.map((data, idx, arr) => (
+                <TimelineItem
+                  data={data}
+                  marker={accumulation}
+                  last={idx > 0 ? arr[idx - 1].timeInMinutes : 0}
+                  index={idx}
+                  nowDone={this.state.nowDone}
+                  key={idx}
+                  />
+                  ))}
+              <TimelineItem
+                data={null}
+                marker={accumulation}
+                last={
+                  this.state.timelineData[this.state.timelineData.length - 1]
+                  .timeInMinutes
+                }
+                index={this.state.timelineData.length}
+                nowDone={this.state.nowDone}
+                key={this.state.timelineData.length}
+                />
+            </div>
+          )}
+        </MediaQuery>
+      </div>
     );
   }
 }
